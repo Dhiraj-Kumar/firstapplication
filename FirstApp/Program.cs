1086c30075f81bc9dad5307f1ca4bbc17945de25
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2020-07-13 14:33:12 +0530

    initial commit

---
+++ FirstApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 8;
+            switch (num)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Sunday");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Monday");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Tuesday");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Wednesday");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("invalid choice entered");
+                        break;
+                    }
+            }
+        }
+    }
+}
